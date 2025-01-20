5e19e7417e1dfd3db7813122b45e3f29c47ac497
Mael <mael@buchs.fr>
2025-01-20 22:41:28 +0100

    added argv number check

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,4 +1,13 @@
 #include "../includes/Server.h"
 #include <arpa/inet.h>
+#include <cstdlib>
 
-int main() { std::cout << "caca" << std::endl; }
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        std::cout << "Error: Invalid argument number" << std::endl;
+        std::cout << "Usage: ./ircserv [port] [password]" << std::endl;
+        exit(1);
+    }
+    std::cout << "Port = " << argv[1] << std::endl;
+    std::cout << "Password = " << argv[2] << std::endl;
+}
