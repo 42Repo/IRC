2d5c801df841b96324f343b0ccfcc78f30f114fe
Mael <mael@buchs.fr>
2025-02-12 12:40:03 +0100

    QUIT command OK

--- srcs/Commands/Quit.cpp
+++ srcs/Commands/Quit.cpp
@@ -1,8 +1,45 @@
 #include "../../includes/CommandHandler.h"
 #include "../../includes/Server.h"
+#include <vector>
+
+static int isSharingChannel(Client *client, Client *target) {
+    std::map<std::string, Channel *> clientChannels = client->getChannels();
+    std::map<std::string, Channel *> targetChannels = target->getChannels();
+
+    // for (std::map<std::string, Channel *>::iterator it = clientChannels.begin();
+    //      it != clientChannels.end(); ++it) {
+    //     for (std::map<std::string, Channel *>::iterator it1 = targetChannels.begin();
+    //          it1 != targetChannels.end(); ++it1) {
+    //         if (it == it1)
+    //             return 1;
+    //     }
+    // }
+    for (std::map<std::string, Channel *>::iterator it = clientChannels.begin();
+         it != clientChannels.end(); ++it) {
+        if (targetChannels[it->first] == it->second)
+            return 1;
+    }
+    return 0;
+}
 
 // TODO - Command - QUIT
 void CommandHandler::handleQuit(Client *client, const std::vector<std::string> &input) {
     std::cout << client->getNickname() << " called QUIT" << std::endl;
     (void)input;
+    Server               *server = client->getServer();
+    std::vector<Client *> clients = server->getClients();
+    for (std::vector<Client *>::iterator it = clients.begin(); it != clients.end(); ++it) {
+        if ((*it)->getNickname() != client->getNickname() && isSharingChannel(client, *it))
+            (*it)->sendMessage(":" + client->getNickname() + "!" + client->getUsername() + "@" +
+                               client->getHostname() + " QUIT :" + input[3] + "\n\r");
+    }
+    
+    std::map<std::string, Channel *> channels = server->getChannels();
+    for (std::map<std::string, Channel *>::iterator it = channels.begin(); it != channels.end(); ++it) {
+        if(it->second->isMember(client))
+        it->second->removeMember(client);
 }
+client->~Client();
+}
+
+//: UserNick!~ident@host QUIT :Quit message
