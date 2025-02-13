dadd620b02df279f28be091bbbbddaa72961c4a6
asuc <sucaurel@gmail.com>
2025-02-13 17:46:23 +0100

    refactor handleJoin method to improve parameter handling and channel joining logic

--- srcs/Commands/Join.cpp
+++ srcs/Commands/Join.cpp
@@ -9,7 +9,6 @@ static std::vector<std::string> splitString(const std::string &str, char delimit
     while (std::getline(ss, token, delimiter)) {
         tokens.push_back(token);
     }
-
     return tokens;
 }
 
@@ -40,86 +39,73 @@ std::string getMemberList(Channel *channel) {
     }
     return memberList;
 }
+
 void CommandHandler::handleJoin(Client *client, const std::vector<std::string> &input) {
-    std::vector<std::string> channels = splitString(input[2], ',');
-    std::vector<std::string> passwords;
-    if (input.size() > 3) {
-        passwords = splitString(input[3], ',');
+    std::vector<std::string> params = splitString(input[2], ' ');
+    std::string              channelName = params[0];
+    std::string              password = (params.size() > 1) ? params[1] : "";
+
+    if (channelName.empty()) {
+        throw Error::IRCError(ERR_NEEDMOREPARAMS("JOIN").c_str());
     }
 
-    if (channels.empty()) {
-        client->sendNumericReply("461", ERR_NEEDMOREPARAMS("JOIN"));
-        return;
+    if (!isValidChannelName(channelName)) {
+        throw Error::IRCError(ERR_BADCHANMASK(channelName).c_str());
     }
 
-    for (size_t i = 0; i < channels.size(); ++i) {
-        std::string channelName = channels[i];
-        std::string password = (i < passwords.size()) ? passwords[i] : "";
+    Channel *channel = _server->getChannelByName(channelName);
+    if (!channel) {
+        channel = new Channel(channelName, client);
+        _server->addChannel(channel);
+        channel->addMember(client);
+        client->joinChannel(channel);
 
-        if (!isValidChannelName(channelName)) {
-            client->sendNumericReply("476", ERR_BADCHANMASK(channelName));
-            continue;
+        std::string joinMessage = ":" + client->getNickname() + "!" + client->getUsername() + "@" +
+                                  client->getHostname() + " JOIN :" + channelName + "\r\n";
+
+        std::map<Client *, std::set<char> > members = channel->getMembers();
+        for (std::map<Client *, std::set<char> >::iterator it = members.begin(); it != members.end();
+             ++it) {
+            Client *member = it->first;
+            member->sendMessage(joinMessage);
         }
 
-        Channel *channel = _server->getChannelByName(channelName);
-        if (!channel) {
-            channel = new Channel(channelName, client);
-            _server->addChannel(channel);
-            channel->addMember(client);
-            client->joinChannel(channel);
+        channel->addChannelMode('t');
+        channel->addUserMode(client, 'o');
+        std::string modeMessage = ":" + _server->getHostname() + " MODE " + channelName + " +t " +
+                                  client->getNickname() + "\r\n";
+        client->sendMessage(modeMessage);
 
-            std::string joinMessage = ":" + client->getNickname() + "!" + client->getUsername() +
-                                      "@" + client->getHostname() + " JOIN :" + channelName +
-                                      "\r\n";
-
-            std::map<Client *, std::set<char> > members = channel->getMembers();
-            for (std::map<Client *, std::set<char> >::iterator it = members.begin();
-                 it != members.end(); ++it) {
-                Client *member = it->first;
-                member->sendMessage(joinMessage);
-            }
-
-            channel->addChannelMode('t');
-            channel->addUserMode(client, 'o');
-            std::string modeMessage =
-                ":" + _server->getHostname() + " MODE " + channelName + " +t " + client->getNickname() + "\r\n";
-            client->sendMessage(modeMessage);
-
-        } else {
-            if (channel->hasChannelMode('i')) {
-                client->sendNumericReply("473", ERR_INVITEONLYCHAN(channelName));
-                continue;
-            }
-            if (channel->getPassword() != "" && channel->getPassword() != password) {
-                client->sendNumericReply("475", ERR_BADCHANNELKEY(channelName));
-                continue;
-            }
-
-            if (channel->getUserLimit() > 0 &&
-                channel->getMembers().size() >= (size_t)channel->getUserLimit()) {
-                client->sendNumericReply("471", ERR_CHANNELISFULL(channelName));
-                continue;
-            }
-
-            channel->addMember(client);
-            client->joinChannel(channel);
-
-            std::string joinMessage = ":" + client->getNickname() + "!" + client->getUsername() +
-                                      "@" + client->getHostname() + " JOIN :" + channelName +
-                                      "\r\n";
-            std::map<Client *, std::set<char> > members = channel->getMembers();
-            for (std::map<Client *, std::set<char> >::iterator it = members.begin();
-                 it != members.end(); ++it) {
-                Client *member = it->first;
-                member->sendMessage(joinMessage);
-            }
+    } else {
+        if (channel->hasChannelMode('i')) {
+            throw Error::IRCError(ERR_INVITEONLYCHAN(channelName).c_str());
+        }
+        if (channel->hasChannelMode('k') && channel->getPassword() != password) {
+            throw Error::IRCError(ERR_BADCHANNELKEY(channelName).c_str());
         }
 
-        // Envoyer RPL_NAMREPLY et RPL_ENDOFNAMES
-        std::string memberList = getMemberList(channel);
-        client->sendMessage(":" + _server->getHostname() + " 353 " + client->getNickname() + " = " +
-                            channelName + " :" + memberList + "\r\n");
-        client->sendMessage(":" + _server->getHostname() + " 366 " + client->getNickname() + " " +
-                            channelName + " :End of /NAMES list\r\n");
+        if (channel->getUserLimit() > 0 &&
+            channel->getMembers().size() >= (size_t)channel->getUserLimit()) {
+            throw Error::IRCError(ERR_CHANNELISFULL(channelName).c_str());
+        }
+
+        channel->addMember(client);
+        client->joinChannel(channel);
+
+        std::string joinMessage = ":" + client->getNickname() + "!" + client->getUsername() + "@" +
+                                  client->getHostname() + " JOIN :" + channelName + "\r\n";
+        std::map<Client *, std::set<char> > members = channel->getMembers();
+        for (std::map<Client *, std::set<char> >::iterator it = members.begin(); it != members.end();
+             ++it) {
+            Client *member = it->first;
+            member->sendMessage(joinMessage);
+        }
     }
+
+    // Envoyer RPL_NAMREPLY et RPL_ENDOFNAMES
+    std::string memberList = getMemberList(channel);
+    client->sendMessage(":" + _server->getHostname() + " 353 " + client->getNickname() + " = " +
+                        channelName + " :" + memberList + "\r\n");
+    client->sendMessage(":" + _server->getHostname() + " 366 " + client->getNickname() + " " +
+                        channelName + " :End of /NAMES list\r\n");
 }
