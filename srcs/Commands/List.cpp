593ea646da62d225ce52406f55214b2a9db12e7d
asuc <sucaurel@gmail.com>
2025-02-14 18:31:39 +0100

    refactor: change intToStr parameter type from int to size_t for better type safety

--- srcs/Commands/List.cpp
+++ srcs/Commands/List.cpp
@@ -1,10 +1,9 @@
 #include "../../includes/CommandHandler.h"
 #include "../../includes/Server.h"
-
 #include <sstream>
 #include <string>
 
-static std::string intToStr(int num) {
+static std::string intToStr(size_t num) {
     std::ostringstream oss;
     oss << num;
     return oss.str();
@@ -15,6 +14,7 @@ void CommandHandler::handleList(Client *client, const std::vector<std::string> &
     Server                          *server = client->getServer();
     std::map<std::string, Channel *> channels = server->getChannels();
 
+    (void)input;
     client->sendNumericReply("321", RPL_LISTSTART(client->getNickname()));
     for (std::map<std::string, Channel *>::iterator it = channels.begin(); it != channels.end();
          ++it) {
